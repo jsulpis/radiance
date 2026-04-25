a7b8c39ee53f713291bd62dfad9fe091c5309f53
Julien Sulpis <code@jsulpis.dev>
2026-04-25 18:14:09 +0200

    chore: add BundleSizeGraph component

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import DefaultTheme from "vitepress/theme";
 import { Sandbox } from "vitepress-plugin-sandpack";
+import BundleSizeGraph from "../../components/BundleSizeGraph.vue";
 import ExampleEditor from "../../components/ExampleEditor.vue";
 import "vitepress-plugin-sandpack/dist/style.css";
 import "virtual:group-icons.css";
@@ -9,6 +10,7 @@ export default {
   ...DefaultTheme,
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx);
+    ctx.app.component("BundleSizeGraph", BundleSizeGraph);
     ctx.app.component("Sandbox", Sandbox);
     ctx.app.component("ExampleEditor", ExampleEditor);
   },
