73986ead0a90b6508315c22c61de483e8d96a435
Julien Sulpis <code@jsulpis.dev>
2026-09-05 14:56:09 +0200

    docs: update the pointer example

--- docs/examples/interactions/pointer/index.ts
+++ docs/examples/interactions/pointer/index.ts
@@ -1,4 +1,4 @@
-import { glCanvas, onPointerEvents } from "@radiancejs/gl";
+import { glCanvas, loop, onPointerEvents } from "@radiancejs/gl";
 import "./styles.css";
 
 const canvas = document.querySelector("canvas")!;
@@ -12,9 +12,9 @@ const { uniforms, onAfterRender } = glCanvas({
     uniform vec3 uCircleColor;
 
     void main() {
-      vec2 uv = (vUv - .5) * uResolution / min(uResolution.x, uResolution.y);
+      vec2 uv = 2. * (vUv - .5) * uResolution / min(uResolution.x, uResolution.y);
       float dist = distance(uv, uPointerPosition);
-      float circleMask = 1. - smoothstep(.099, .101, dist);
+      float circleMask = 1. - smoothstep(.137, .143, dist);
       vec3 circle = mix(vec3(0.), uCircleColor, circleMask);
       gl_FragColor = vec4(circle, 1.);
     }
@@ -26,12 +26,14 @@ const { uniforms, onAfterRender } = glCanvas({
   },
 });
 
+const targetPointer = { x: 0, y: 0 };
+const speed = 0.07;
+
 onPointerEvents(canvas, {
   move: ({ pointer, boundingRect, center }) => {
-    uniforms.uPointerPosition = [
-      (pointer.x - center.x) / Math.min(boundingRect.width, boundingRect.height),
-      (center.y - pointer.y) / Math.min(boundingRect.width, boundingRect.height),
-    ];
+    const minDimension = Math.min(boundingRect.width, boundingRect.height);
+    targetPointer.x = (pointer.x - center.x) / (minDimension / 2);
+    targetPointer.y = (center.y - pointer.y) / (minDimension / 2);
   },
   down: () => {
     uniforms.uCircleColor = [1, 0, 0];
@@ -39,6 +41,28 @@ onPointerEvents(canvas, {
   up: () => {
     uniforms.uCircleColor = [1, 1, 1];
   },
+  leave: () => {
+    targetPointer.x = 0;
+    targetPointer.y = 0;
+  },
+});
+
+loop(() => {
+  const currentPointerCoord = {
+    x: uniforms.uPointerPosition[0],
+    y: uniforms.uPointerPosition[1],
+  };
+  const distanceX = targetPointer.x - currentPointerCoord.x;
+  const distanceY = targetPointer.y - currentPointerCoord.y;
+
+  if (Math.abs(distanceX) < 0.001 && Math.abs(distanceY) < 0.001) {
+    return;
+  }
+
+  uniforms.uPointerPosition = [
+    currentPointerCoord.x + distanceX * speed,
+    currentPointerCoord.y + distanceY * speed,
+  ];
 });
 
 const renderCount = document.querySelector("#renderCount")!;
