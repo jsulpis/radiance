b35e57198ffb8fbbcabe1a6dd62f4d817bfd7ce9
Julien Sulpis <code@jsulpis.dev>
2026-09-05 13:57:58 +0200

    docs: update the play/pause example

--- docs/examples/basics/play-pause/index.ts
+++ docs/examples/basics/play-pause/index.ts
@@ -1,32 +1,31 @@
 import { glCanvas } from "@radiancejs/gl";
+import fragment from "./metaballs.frag?raw";
 import "./styles.css";
 
-const { play, pause, canvas, onAfterRender } = glCanvas({
+const { play, pause, onAfterRender } = glCanvas({
   canvas: "#glCanvas",
-  fragment: /* glsl */ `
-    varying vec2 uv;
-    uniform float uTime;
-    #define RADIUS .1
-
-    void main() {
-      vec2 circleCenter = vec2(cos(uTime * .8) * .25 + .5, sin(uTime * .8) * .25 + .5);
-      float circleMask = 1. - smoothstep(RADIUS*.99, RADIUS*1.01, length(uv - circleCenter));
-      float gradient = 1. - length(uv - RADIUS - circleCenter) / (RADIUS * 2.);
-      vec3 color = mix(vec3(0.1, 0.6, 0.4), vec3(0.41, 0.98, 0.84), gradient) * circleMask;
-
-      gl_FragColor = vec4(color, 1.);
-    }
-  `,
+  fragment,
   uniforms: {
-    uTime: ({ time }) => time / 500,
+    uTime: ({ time }) => time / 1000,
   },
-  immediate: false,
+  immediate: false, // prevent the loop from starting immediately
+});
+
+const playPauseButton = document.querySelector<HTMLButtonElement>("#playPause")!;
+let isPlaying = false;
+
+playPauseButton.addEventListener("click", () => {
+  isPlaying = !isPlaying;
+  playPauseButton.classList.toggle("playing", isPlaying);
+  playPauseButton.setAttribute("aria-label", isPlaying ? "Pause" : "Play");
+  if (isPlaying) {
+    play();
+  } else {
+    pause();
+  }
 });
 
 const renderCount = document.querySelector("#renderCount")!;
 onAfterRender(() => {
   renderCount.firstChild!.nodeValue = `${Number(renderCount.textContent) + 1}`;
 });
-
-canvas.addEventListener("pointerenter", play);
-canvas.addEventListener("pointerleave", pause);
